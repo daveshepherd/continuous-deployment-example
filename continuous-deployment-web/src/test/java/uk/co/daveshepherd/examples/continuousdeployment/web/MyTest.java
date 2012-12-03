6a9c16af9f6a4b3bc2e7dc9217b1ff0ebb39a072
Dave Shepherd <dave.shepherd@endor.me.uk>
2012-12-03 17:23:30 +0000

    Add a simple test for demonstration purposes

---
+++ continuous-deployment-web/src/test/java/uk/co/daveshepherd/examples/continuousdeployment/web/MyTest.java
@@ -0,0 +1,13 @@
+package uk.co.daveshepherd.examples.continuousdeployment.web;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void testSomething() {
+        assertTrue("some test", true);
+    }
+}
