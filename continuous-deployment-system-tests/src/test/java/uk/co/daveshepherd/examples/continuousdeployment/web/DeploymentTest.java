9c78956898cd91a6dfe62a459d6d085da50e6e93
Dave Shepherd <dave.shepherd@endor.me.uk>
2012-12-04 16:53:11 +0000

    Add retry to deployment test to cater for delay in application start up

--- continuous-deployment-system-tests/src/test/java/uk/co/daveshepherd/examples/continuousdeployment/web/DeploymentTest.java
+++ continuous-deployment-system-tests/src/test/java/uk/co/daveshepherd/examples/continuousdeployment/web/DeploymentTest.java
@@ -1,6 +1,9 @@
 package uk.co.daveshepherd.examples.continuousdeployment.web;
 
-import static org.junit.Assert.assertEquals;
+import static java.lang.Thread.sleep;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.Callable;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -12,23 +15,54 @@ public class DeploymentTest {
 
     @Test
     public void testDeployment() throws Exception {
-      
-       
-       
+
         final HttpGet request = new HttpGet("http://ds-dev.j.layershift.co.uk/continous-deployment/");
         try {
             final DefaultHttpClient httpclient = new DefaultHttpClient();
 
-            final HttpResponse response = httpclient.execute(request);
+            retry(new RetryCall() {
+                public Boolean call() {
+                    HttpResponse response;
+                    try {
+                        response = httpclient.execute(request);
+                    } catch (final Exception e) {
+                        System.err.println("Exception executing http request: " + e.getMessage());
+                        return false;
+                    }
 
-            int status = -1;
+                    final int status = response.getStatusLine().getStatusCode();
 
-            status = response.getStatusLine().getStatusCode();
+                    System.out.println("HTTP status code: " + status);
+                    return HttpStatus.SC_OK == status;
+                }
+            });
 
-            assertEquals("status should be success", HttpStatus.SC_OK, status);
         } finally {
             request.releaseConnection();
         }
     }
 
+    public void retry(final RetryCall block) throws InterruptedException {
+
+        int retry = 8;
+        long intervalBetweenRetriesInMillis = 1000;
+
+        for (; retry > 0; retry--) {
+            System.out.println("Retry block, iteration: " + retry);
+            if (block.call()) {
+                return;
+            }
+            sleep(intervalBetweenRetriesInMillis);
+            intervalBetweenRetriesInMillis = intervalBetweenRetriesInMillis * 2;
+        }
+        fail("failed to do retry");
+    }
+
+    private interface RetryCall extends Callable<Boolean> {
+
+        /**
+         * @return true if the call was successful
+         */
+        Boolean call();
+    }
 }
